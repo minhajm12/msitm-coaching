1a6f812cd72b2a2f35c3d6809165d41ed78e8d63
minhajm12 <minhajatholi222@gmail.com>
2024-04-28 00:23:05 +0530

    new

---
+++ src/components/Footer.js
@@ -0,0 +1,163 @@
+import React from 'react'
+
+import footerLogo from './../assets/img/logo/msitm-logo-footer.svg';
+import playStore from './../assets/img/svg/footer/play-store.svg';
+import appleStore from './../assets/img/svg/footer/apple-store.svg';
+import locationIcn from './../assets/img/svg/footer/location.svg';
+import phoneIcn from './../assets/img/svg/footer/phone.svg';
+import mailIcn from './../assets/img/svg/footer/email.svg';
+import fbIcn from './../assets/img/svg/footer/facebook.svg';
+import youtubeIcn from './../assets/img/svg/footer/youtube.svg';
+import twitterIcn from './../assets/img/svg/footer/twitter.svg';
+import instaIcn from './../assets/img/svg/footer/instagram.svg';
+
+function footer() {
+  return (
+    <div className='footer-sec'>
+      <div className="footer-cta-sec">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6 col-12 left">
+              <h2>Don't let your STEM degree<br /> go to waste!</h2>
+              <a href="" className="main-btn">Apply Now</a>
+            </div>
+            <div className="col-lg-6 col-12 right"></div>
+          </div>
+        </div>
+      </div>
+      <div className="footer-main">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4 col-md-6 col-12 logo-div">
+              <div className="logo-img-div">
+                <img src={footerLogo} alt="" className='img-fluid' />
+              </div>
+              <div className="newsletter-div">
+                <h6>Join Our Newsletter</h6>
+                <p className="main-para">Be update with our news and updates</p>
+                <form action="">
+                  <input type="email" className='newsletter' placeholder='Enter Email Address' />
+                  <button>Submit</button>
+                </form>
+              </div>
+              <div className="download-app">
+                <h6>Download Our App Now</h6>
+                <div className="app-sec">
+                  <a href="">
+                    <img src={playStore} alt="" className='img-fluid' />
+                  </a>
+                  <a href="">
+                    <img src={appleStore} alt="" className='img-fluid' />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-12 footer-nav ps-md-5">
+              <h4>Usefull Links</h4>
+              <ul>
+                <li>
+                  <a href="">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    Programe
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    MSITM Mentoring
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    Mentoring
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    News & Updates
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-lg-2 col-md-6 col-12 footer-nav mt-md-5 ">
+              <h4>Usefull Links</h4>
+              <ul>
+                <li>
+                  <a href="">
+                    Technology
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    Management
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    Leadership
+                  </a>
+                </li>
+                <li>
+                  <a href="">
+                    Mentorship
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-lg-3 col-md-6 col-12 contact-details mt-md-5 ps-md-5">
+              <div className="contact">
+                <h4>Contact Details</h4>
+                <div className="items">
+                  <a href="">
+                    <img src={locationIcn} alt="" className='img-fluid location' />
+                    <p>Lorem Ipsum is simply dummy text of the printing and</p>
+                  </a>
+                  <a href="">
+                    <img src={phoneIcn} alt="" className='img-fluid' />
+                    <p>+(570) 1231 456 748</p>
+                  </a>
+                  <a href="">
+                    <img src={mailIcn} alt="" className='img-fluid' />
+                    <p>info@msitmcoach.com</p>
+                  </a>
+                </div>
+              </div>
+              <div className="social-icns mt-md-5">
+                <div className="items">
+                  <h4>Follow Us On</h4>
+                  <div className="icons">
+                    <a href="">
+                      <img src={fbIcn} alt="" className='img-fluid' />
+                    </a>
+                    <a href="">
+                      <img src={youtubeIcn} alt="" className='img-fluid' />
+                    </a>
+                    <a href="">
+                      <img src={twitterIcn} alt="" className='img-fluid' />
+                    </a>
+                    <a href="">
+                      <img src={instaIcn} alt="" className='img-fluid' />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="copyright">
+        <p className='main-para text-center'>© 2024 MSITMCoaching. All Right Reserved.</p>
+      </div>
+    </div>
+  )
+}
+
+export default footer
---
+++ src/components/Header.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import { useEffect } from 'react';
+
+import HeaderLogo from './../assets/img/logo/msitm-logo-header.svg';
+import menubar from './../assets/img/svg/menu-bars.svg';
+
+function Header() {
+
+
+
+    useEffect(() => {
+        const headerHeight = document.querySelector('.header-main').offsetHeight;
+        document.documentElement.style.setProperty('--header-height', `${headerHeight}px`);
+        console.log(headerHeight);
+    }, []);
+
+
+
+    return (
+
+        <div className='header-main' id='header-main'>
+            <nav class="navbar navbar-expand-lg">
+                <div class="container-fluid d-flex">
+                    <a href="" className='header-logo'>
+                        <img src={HeaderLogo} alt="" />
+                    </a>
+
+                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+                        {/* <span class="navbar-toggler-icon"></span> */}
+                        <img src={menubar} alt=""  className='img-fluid'/>
+                    </button>
+
+                    <div class="offcanvas offcanvas-start d-lg-none d-md-block" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+                        <div class="offcanvas-header">
+                            <a href="">
+                                <img src={HeaderLogo} alt="" />
+                            </a>
+                            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div class="navbar-nav">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link" href="#">Programs</a>
+                            <a class="nav-link" href="#">Hire From MSITM</a>
+                            <a class="nav-link" href="#">Mentoring</a>
+                            <a class="nav-link" href="#">Blog</a>
+                            <a class="main-btn" href="#">Free Consultation</a>
+                        </div>
+                    </div>
+
+                    <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link" href="#">Programs</a>
+                            <a class="nav-link" href="#">Hire From MSITM</a>
+                            <a class="nav-link" href="#">Mentoring</a>
+                            <a class="nav-link" href="#">Blog</a>
+                            <a class="main-btn" href="#">Free Consultation</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+    )
+}
+
+export default Header
